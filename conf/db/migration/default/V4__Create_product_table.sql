d8b41c21909e069f4b985251acba498b82d8eea5
Deny Prasetyo <jasoet87@gmail.com>
2016-04-21 02:03:29 +0700

    Add Product Table

---
+++ conf/db/migration/default/V4__Create_product_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE categories (
+  code        VARCHAR(150) PRIMARY KEY   NOT NULL,
+  name        VARCHAR(150) UNIQUE        NOT NULL,
+  description VARCHAR(500)               NOT NULL,
+  color       VARCHAR(150)               NOT NULL,
+  size        VARCHAR(5)                 NOT NULL,
+  picture_url VARCHAR(150)               NOT NULL,
+  price       INTEGER                    NOT NULL,
+  category_id INTEGER                    NOT NULL,
+  CONSTRAINT category_id_fk FOREIGN KEY (category_id) REFERENCES categories (id)
+);
