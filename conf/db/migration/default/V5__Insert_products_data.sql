baf16e177e2c84ddc14122a8160f7bc162bea3d0
Deny Prasetyo <jasoet87@gmail.com>
2016-04-21 09:41:53 +0700

    Insert Products data

---
+++ conf/db/migration/default/V5__Insert_products_data.sql
@@ -0,0 +1,200 @@
+INSERT INTO products (code, name, description, color, size, picture_url, price, category_id)
+VALUES
+  (
+    'veendeva-plain-flare-maxi-dress',
+    'Veendeva Plain Flare Maxi Dress',
+    '
+    Satu ukuran muat sampai size L
+    Lingkar dada 96 cm, Panjang 145 cm, Lebar bahu 39 cm, Panjang lengan 58 cm, Lingkar lengan 36 cm, Lingkar pinggang 96 cm
+    Bahan Katun Rayon
+    1 Kancing siku, 1 Kancing ujung lengan, 5 Kancing depan, free belt
+    ',
+    'Red',
+    'L',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/8a2e77390320a8313cc27d8bdf81ad3c_600.jpg',
+    98000,
+    1
+  ),
+  (
+    'nelfianti-plain-flare-maxi-dress',
+    'Nelfianti Plain Flare Maxi Dress',
+    '
+    Satu ukuran muat sampai size L kecil
+Lingkar dada 95 cm, Panjang 139 cm
+Bahan Katun Rayon
+Resleting Belakang, Ikat Pinggang
+    ',
+    'Light Blue, Blue, Dark Blue, Navy, dan Black',
+    'L',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/a6529e81f31fa667bae50028f19365d9_600.jpg',
+    99000,
+    2
+  ),
+  (
+    'zarziza-plain-flare-maxi-dress',
+    'Zarziza Plain Flare Maxi Dress',
+    '
+    Satu ukuran muat sampai size M
+Lingkar dada 87 cm, Lebar bahu 37 cm, Panjang Lengan 36 Cm, Panjang 135 cm, Lingkar pinggul 96 cm,
+Bahan Katun Rayon
+Resleting belakang, Pinggang Karet Full
+    ',
+    'Navy, Choco, Tangerine, Pink, Peach, Black',
+    'M',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/e78b342ff67489d69bdbdd474592a7eb_600.jpg',
+    99000,
+    3
+  ),
+  (
+    'chyntya-plain-jogger-long-pants',
+    'Chyntya Plain Jogger Long Pants',
+    '
+    (kiri--kanan) Warna Dark Brown, Khaki, dan Black
+Satu ukuran muat sampai size S
+Lingkar pinggang 72 cm, Panjang 93 cm, Lingkar pinggul 90 cm
+Bahan Katun Stretch
+Pinggang full karet, 2 Saku depan, Tali pinggang
+    ',
+    'White',
+    'S',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/1086cb47d47e914611dda407b7efa217_600.jpg',
+    75000,
+    4
+  ),
+  (
+    'zeaniavia-plain-v-wrap-blouse',
+    'Zeaniavia Plain V-Wrap Blouse',
+    '
+   Satu ukuran muat sampai size L kecil
+Lingkar dada 92 cm, Lebar bahu 34 cm, Lingkar lengan 40 cm, Panjang 65 cm,
+Lingkar Pinggang 88 cm
+Bahan Katun Stretch
+    ',
+    'Navy dan Black',
+    'L',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/e6fb6b974544493bd02126379e7bcfa2_600.jpg',
+    85000,
+    5
+  ),
+
+  (
+    'shyseera-plain-peplum-blouse',
+    'Shyseera Plain Peplum Blouse',
+    '
+Satu ukuran muat sampai size S
+Lingkar dada 84 cm, Lebar bahu 34 cm, Lingkar pinggul 92 cm
+Lingkar lengan 28 cm, Panjang 59 cm, Lingkar pinggang 78 cm, Lingkar pinggul 82 cm
+Bahan Katun Stretch
+Resleting belakang
+    ',
+    'Purple, Navy, dan Tosca',
+    'M',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/f0b1b3b79085c205ef62badcba4a71cd_600.jpg',
+    85000,
+    6
+  ),
+
+  (
+    'miandia-plain-peplum-blouse',
+    'Miandia Plain Peplum Blouse',
+    '
+Satu ukuran muat sampai size S
+Lingkar dada 82 cm, Lebar bahu 35 cm, Lingkar lengan 40 cm,
+Panjang 58 cm, Lingkar pinggang 70 cm
+Bahan Katun Stretch
+Resliting Belakang
+    ',
+    'Black',
+    'XL',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/a6529e81f31fa667bae50028f19365d9_600.jpg',
+    70000,
+    7
+  ),
+
+  (
+    'nashiralia-abstract-layer-mini-dress',
+    'Nashiralia Abstract Layer Mini Dress',
+    '
+    Satu ukuran muat sampai size L kecil
+Lingkar dada 95 cm, Panjang 139 cm
+Bahan Katun Rayon
+Resleting Belakang, Ikat Pinggang
+    ',
+    'Fuchsia, Navy, Black, dan White',
+    'M',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/4fd8165a5cf7a115ed56a4b8bf6855f1_600.jpg',
+    103000,
+    8
+  ),
+
+  (
+    'bronte-flowery-layer-mini-dress',
+    'Bronte Flowery Layer Mini Dress',
+    '
+Satu ukuran muat sampai size L
+Lingkar dada 89-99 cm, Panjang 85 cm
+Bahan Wedges Scuba
+Resleting belakang
+    ',
+    'Tangerine, Pink, Teal, dan Yellow',
+    'XXL',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/a6529e81f31fa667bae50028f19365d9_600.jpg',
+    99000,
+    9
+  ),
+
+
+  (
+    'teziielo-plain-casual-mini-dress',
+    'Teziielo Plain Casual Mini Dress',
+    '
+Satuan Ukuran Muat Sampai Size M
+Lingkar dada 86 cm, Lebar bahu 38 cm, Lingkar lengan 42 cm, Panjang 92 cm, Lingkar pinggang 90 cm
+Bahan Wedges
+    ',
+    'White, Light Blue, Baby Pink, dan Grey',
+    'M',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/334161335728a478caddc60c98f23a80_600.jpg',
+    99000,
+    10
+  ),
+
+
+  (
+    'collathe-plain-flare-mini-dress',
+    'Collathe Plain Flare Mini Dress',
+    '
+    Satu ukuran muat sampai size L kecil
+Lingkar dada 95 cm, Panjang 139 cm
+Bahan Katun Rayon
+Resleting Belakang, Ikat Pinggang
+    ',
+    'Light Blue, Grey, Baby Pink, dan White',
+    'XS',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/30e0c73d219cde988b9e77b70ab28377_600.jpg',
+    112000,
+    11
+  ),
+
+
+  (
+    'shizila-ethnic-casual-couple-set',
+    'Shizila Ethnic Casual Couple Set',
+    '
+Pria:
+Lingkar dada 98 cm, Lebar bahu 43 cm, Lingkar lengan 50 cm, Panjang 69 cm, Lingkar pinggang 92 cm
+Bahan Katun Stretch
+6 Kancing depan, 1 Saku depan
+Wanita:
+Lingkar dada 72 cm, Lebar bahu 32 cm, Lingkar lengan 46 cm, Panjang 87 cm, Lingkar pinggang 68 cm
+Resleting belakang
+Bahan Katun Stretch
+    ',
+    'Black, Navy, Tosca, dan Maroon',
+    'M',
+    'https://salestock-public-prod.global.ssl.fastly.net/product_images/9a98fd65286e31e3e5af1ffac270ce1b_600.jpg',
+    179000,
+    12
+  )
+
+
