20bf23c43113de19c387003d7275435b030dbb80
Deny Prasetyo <jasoet87@gmail.com>
2016-04-21 21:35:33 +0700

    Implement Controller - Rest Api for Category - Rest Api for Product

--- app/controllers/CategoryController.scala
+++ app/controllers/CategoryController.scala
@@ -2,8 +2,12 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.mvc.Controller
-import services.{CategoryService, ProductService}
+import domains.Category
+import domains.Category._
+import play.api.Logger
+import play.api.libs.json._
+import play.api.mvc.{Controller, _}
+import services.CategoryService
 
 /**
   * Controller Class to Serve Category Rest API
@@ -14,13 +18,96 @@ import services.{CategoryService, ProductService}
 @Singleton
 class CategoryController @Inject()(service: CategoryService) extends Controller {
 
-  def list() = play.mvc.Results.TODO
+  def list() = Action {
+    service.findAll()
+      .map { list => Ok(Json.toJson(list)) }
+      .recover {
+        case ex =>
+          Logger.error(ex.getMessage, ex)
+          InternalServerError(ex.getMessage)
+      }
+      .get
+  }
 
-  def get(code: String) = play.mvc.Results.TODO
+  def get(id: Int) = Action {
+    service.findOne(id)
+      .map {
+        _
+          .map { c => Ok(Json.toJson(c)) }
+          .getOrElse(NotFound)
+      }
+      .recover {
+        case e =>
+          Logger.error(e.getMessage, e)
+          InternalServerError(e.getMessage)
+      }
+      .get
+  }
 
-  def create() = play.mvc.Results.TODO
+  def create() = Action { request =>
+    request.body.asJson
+      .map {
+        _.validate[Category]
+          .map { category =>
+            service.insert(category)
+              .map { _ =>
+                Ok("Category Successfully Created!")
+              }
+              .recover {
+                case e =>
+                  Logger.error(s"Create Error: ${e.getMessage}", e)
+                  BadRequest(e.getMessage)
+              }
+              .get
+          }
+          .recoverTotal {
+            e =>
+              Logger.error(s"Validate Json Error: ${e.errors.mkString(",")}")
+              BadRequest(JsError.toJson(e))
+          }
+      }
+      .getOrElse {
+        BadRequest("Expecting Json Data")
+      }
+  }
 
-  def modify(code: String) = play.mvc.Results.TODO
+  def modify(id: Int) = Action { request =>
+    request.body.asJson
+      .map {
+        _.validate[Category]
+          .map { category =>
+            service.update(id, category)
+              .map { _ =>
+                Ok("Category Successfully Updated!")
+              }
+              .recover {
+                case e =>
+                  Logger.error(s"Update Error: ${e.getMessage}", e)
+                  BadRequest(e.getMessage)
+              }
+              .get
+          }
+          .recoverTotal {
+            e =>
+              Logger.error(s"Validate Json Error: ${e.errors.mkString(",")}")
+              BadRequest(JsError.toJson(e))
+          }
+      }
+      .getOrElse {
+        BadRequest("Expecting Json data")
+      }
+  }
 
-  def delete(code: String) = play.mvc.Results.TODO
+  def delete(id: Int) = Action {
+    service.delete(id)
+      .map { affectedRow =>
+        Ok(s"Delete $affectedRow rows Successfully!!")
+      }
+      .recover {
+        case e =>
+          Logger.error(s"Delete Category Error: ${e.getMessage}", e)
+          InternalServerError(e.getMessage)
+      }
+      .get
+  }
 }
--- app/controllers/ProductController.scala
+++ app/controllers/ProductController.scala
@@ -2,7 +2,11 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.mvc.Controller
+import domains.Product
+import domains.Product._
+import play.api.Logger
+import play.api.libs.json.{JsError, Json}
+import play.api.mvc.{Controller, _}
 import services.ProductService
 
 /**
@@ -14,13 +18,96 @@ import services.ProductService
 @Singleton
 class ProductController @Inject()(service: ProductService) extends Controller {
 
-  def list() = play.mvc.Results.TODO
+  def list() = Action {
+    service.findAll()
+      .map { list => Ok(Json.toJson(list)) }
+      .recover {
+        case ex =>
+          Logger.error(ex.getMessage, ex)
+          InternalServerError(ex.getMessage)
+      }
+      .get
+  }
 
-  def get(code: String) = play.mvc.Results.TODO
+  def get(code: String) = Action {
+    service.findOne(code)
+      .map {
+        _
+          .map { c => Ok(Json.toJson(c)) }
+          .getOrElse(NotFound)
+      }
+      .recover {
+        case e =>
+          Logger.error(e.getMessage, e)
+          InternalServerError(e.getMessage)
+      }
+      .get
+  }
 
-  def create() = play.mvc.Results.TODO
+  def create() = Action { request =>
+    request.body.asJson
+      .map {
+        _.validate[Product]
+          .map { product =>
+            service.insert(product)
+              .map { _ =>
+                Ok("Product Successfully Created!")
+              }
+              .recover {
+                case e =>
+                  Logger.error(s"Create Error: ${e.getMessage}", e)
+                  BadRequest(e.getMessage)
+              }
+              .get
+          }
+          .recoverTotal {
+            e =>
+              Logger.error(s"Validate Json Error: ${e.errors.mkString(",")}")
+              BadRequest(JsError.toJson(e))
+          }
+      }
+      .getOrElse {
+        BadRequest("Expecting Json Data")
+      }
+  }
 
-  def modify(code: String) = play.mvc.Results.TODO
+  def modify(code: String) = Action { request =>
+    request.body.asJson
+      .map {
+        _.validate[Product]
+          .map { product =>
+            service.update(code, product)
+              .map { _ =>
+                Ok("Product Successfully Updated!")
+              }
+              .recover {
+                case e =>
+                  Logger.error(s"Update Error: ${e.getMessage}", e)
+                  BadRequest(e.getMessage)
+              }
+              .get
+          }
+          .recoverTotal {
+            e =>
+              Logger.error(s"Validate Json Error: ${e.errors.mkString(",")}")
+              BadRequest(JsError.toJson(e))
+          }
+      }
+      .getOrElse {
+        BadRequest("Expecting Json data")
+      }
+  }
 
-  def delete(code: String) = play.mvc.Results.TODO
+  def delete(code: String) = Action {
+    service.delete(code)
+      .map { affectedRow =>
+        Ok(s"Delete $affectedRow rows Successfully!!")
+      }
+      .recover {
+        case e =>
+          Logger.error(s"Delete Product Error: ${e.getMessage}", e)
+          InternalServerError(e.getMessage)
+      }
+      .get
+  }
 }
